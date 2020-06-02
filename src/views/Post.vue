8fb7b799aab4853b1032545113286c8a2369f0bc
Sacky <yuji08752@gmail.com>
2020-06-03 01:05:05 +0900

    MarkDownの投稿を表示

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -1,41 +1,16 @@
 <template>
-  <div id="app">
-    <div id="main">
-      <mavon-editor v-model="value" :toolbars="markdownOption" language="ja" />
-    </div>
-    <button v-on:click="post">投稿</button>
+  <div id="App">
+    <PostForm />
+    <PostList />
   </div>
 </template>
-
 <script>
-import Vue from "vue";
-import mavonEditor from "mavon-editor";
-import "mavon-editor/dist/css/index.css";
-import { db } from "@/firebase";
-
-Vue.use(mavonEditor);
+import PostForm from "@/components/PostForm";
 
 export default {
-  name: "app",
-  data() {
-    return {
-      value: "",
-      markdownOption: {
-        bold: true,
-        italic: true,
-        header: true,
-      }
-    }
-  },
-  methods: {
-    post() {
-      const post = {
-        value: this.value
-      }
-      db.collection("posts")
-        .add(post)
-    }
-  },
+  name: "App",
+  components: {
+    PostForm
+  }
 };
-
 </script>
