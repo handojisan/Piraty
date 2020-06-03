856fb939679d3d44e84921749650de7787f94da7
川端裕紀 <kawabata@kawabatayuukinoMacBook-ea.local>
2020-06-03 17:56:03 +0900

    save

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,21 +1,21 @@
 <template>
   <div>
- <!---最後のinputタグのvalueの中に自分のサイトのドメイン名をいれる-->
+    <!---最後のinputタグのvalueの中に自分のサイトのドメイン名をいれる-->
     <form method="get" action="http://www.google.co.jp/search" target="_blank">
       <input type="text" name="q" size="31" maxlength="255" value="" />
       <input type="submit" name="btng" value="検索" />
       <input type="hidden" name="hl" value="ja" />
-      <input type="hidden" name="sitesearch" value="" />
+      <input
+        type="hidden"
+        name="sitesearch"
+        value="https://piraty-project.firebaseapp.com/"
+      />
     </form>
- </div>
+  </div>
 </template>
 
 <script>
-  export default {
-
-  }
+export default {};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
