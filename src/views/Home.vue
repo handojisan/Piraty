b43ddb0164037a8133a2c6314f592d394d0edbd3
sakaguchi <tsaka311@gmail.com>
2020-06-07 18:16:20 +0900

    ヘッダー

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,25 @@
 <template>
-  <div id="app">
-    <div id="box">
-      <ul>
-        <span>投稿一覧</span>
-        <li>
-          カテゴリー
-          <select name="category">
-            <option value="サンプル1">サンプル1</option>
-            <option value="サンプル2">サンプル2</option>
-            <option value="サンプル3">サンプル3</option>
-          </select>
-        </li>
-        <li>
-          順番
-          <select name="dateorder">
-            <option value="サンプル1">投稿日順</option>
-            <option value="サンプル2">サンプル2</option>
-            <option value="サンプル3">サンプル3</option>
-          </select>
-        </li>
-        <li><input type="submit" name="submit" value="検索" /></li>
-      </ul>
-    </div>
+  <div class="box">
+    <ul>
+      <span>投稿一覧</span>
+      <li>
+        カテゴリー
+        <select name="category">
+          <option value="サンプル1">サンプル1</option>
+          <option value="サンプル2">サンプル2</option>
+          <option value="サンプル3">サンプル3</option>
+        </select>
+      </li>
+      <li>
+        順番
+        <select name="dateorder">
+          <option value="サンプル1">投稿日順</option>
+          <option value="サンプル2">サンプル2</option>
+          <option value="サンプル3">サンプル3</option>
+        </select>
+      </li>
+      <li><input type="submit" name="submit" value="検索" /></li>
+    </ul>
     <Search />
     <PostList />
     <!-- ↑投稿を表示する -->
@@ -39,6 +37,9 @@ export default {
 };
 </script>
 <style lang="scss">
+.box {
+  padding: 30px;
+}
 ul {
   margin: 0px 0px 0px 30px;
   padding-left: 0px;
