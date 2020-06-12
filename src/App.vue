7a5e4ddf7515236f881884267e0f0d27de9c313c
川端裕紀 <kawabata@kawabatayuukinoMacBook-ea.local>
2020-06-13 02:25:49 +0900

    profileChange

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,63 @@
 <template>
   <div id="app">
     <header>
-      <div id="container-example2">
-        <div class="strip colour-1">
-          <div v-if="user" class="user-app">
-            <router-link to="/profile"
-              >{{ user.email }}でログイン中</router-link
-            >
-            <a v-on:click="SignOut">Logout</a>
+      <!-- <div id="container-example2">
+        <div class="strip colour-1"> -->
+      <div class="background">
+        <div class="hanburger">
+          <a
+            class="menu-trigger"
+            v-bind:class="{ active: isclicked }"
+            v-on:click="isclicked = !isclicked"
+            href="#"
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </a>
+          <div class="menuLine" v-show="isclicked">
+            <div v-if="user" class="user-app">
+              <div class="user-email">
+                <router-link to="/profile">{{ user.email }}</router-link>
+              </div>
+              <div class="userLogout">
+                <a v-on:click="SignOut">Logout</a>
+              </div>
+            </div>
+            <div v-else>
+              <router-link to="/login">SignIn Login</router-link>
+            </div>
+            <!-- <div id="container-example2">
+              <div class="strip colour-1"> -->
           </div>
-          <div v-else>
-            <router-link to="/login">SignIn Login</router-link>
-          </div>
+        </div>
+
+        <div class="title">
+          <vue-particles
+            class="particles"
+            color="#FFFF99"
+            :particleOpacity="1"
+            :particlesNumber="200"
+            shapeType="circle"
+            :particleSize="1"
+            linesColor="#FFCC66"
+            :linesWidth="4"
+            :lineLinked="true"
+            :lineOpacity="0.7"
+            :linesDistance="180"
+            :moveSpeed="5"
+            :hoverEffect="true"
+            hoverMode="grab"
+            :clickEffect="true"
+            clickMode="push"
+          >
+          </vue-particles>
+          <div class="titleName"><h1>PIRATY</h1></div>
         </div>
       </div>
     </header>
-
-    <div class="title">
-      <h1>PIRATY</h1>
-    </div>
     <div id="container-example">
-      <div class="strip colour-1">
+      <div class="menu-bar colour-1">
         <router-link to="/">Top</router-link>
         <router-link to="/about">About</router-link>
         <router-link to="/ranking">Ranking</router-link>
@@ -40,7 +77,8 @@ import firebase from "firebase";
 export default {
   data() {
     return {
-      user: null
+      user: null,
+      isclicked: false
     };
   },
   methods: {
@@ -66,13 +104,12 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkdit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  //background-image: url("./assets/image/space.jpg");
   text-align: center;
   color: #2c3e50;
+  position: relative;
 }
-.title {
-  text-align: center;
-  font-size: 28px;
-}
+
 #nav {
   padding: 30px;
   a {
@@ -84,56 +121,125 @@ export default {
     }
   }
 }
-#container-example2 {
+// #container-example2 {
+//   position: relative;
+
+//   *,
+//   *::after,
+//   *::before {
+//     box-sizing: border-box;
+//   }
+//   .strip {
+//     width: 100%;
+//     height: 80px;
+//     display: flex;
+//     flex-wrap: wrap;
+//     align-items: center;
+//     justify-content: space-around;
+//     background: #ffcc99;
+//   }
+//   a {
+//     font-size: 10pt;
+//     font-family: sans-serif;
+//     color: #fff;
+//     text-decoration: none;
+//     letter-spacing: 2px;
+//     position: relative;
+//     text-align: right;
+//     padding: 15px 50px;
+//     width: 300px;
+//   }
+
+//   .colour-1 {
+//     background: #ffcc99;
+//     a::after {
+//       width: 100%;
+//       height: 0;
+//       background: #fff;
+//       content: "";
+//       position: absolute;
+//       top: 75%;
+//       left: 0;
+//       opacity: 0;
+//       transition: all 0.3s ease-out;
+//       margin: auto;
+//     }
+//     a:hover::after {
+//       opacity: 1;
+//       top: 80%;
+//       height: 2px;
+//     }
+//   }
+// }
+.menu-trigger,
+.menu-trigger span {
+  display: inline-block;
+  transition: all 0.4s;
+  box-sizing: border-box;
+}
+.menu-trigger {
   position: relative;
-
-  *,
-  *::after,
-  *::before {
-    box-sizing: border-box;
-  }
-  .strip {
-    width: 100%;
-    height: 80px;
-    display: flex;
-    flex-wrap: wrap;
-    align-items: center;
-    justify-content: space-around;
-    background: orange;
-  }
-  a {
-    font-size: 10pt;
-    font-family: sans-serif;
-    color: #fff;
-    text-decoration: none;
-    letter-spacing: 2px;
-    position: relative;
-    text-align: right;
-    padding: 15px 50px;
-    width: 300px;
-  }
-
-  .colour-1 {
-    background: orange;
-    a::after {
-      width: 100%;
-      height: 0;
-      background: #fff;
-      content: "";
-      position: absolute;
-      top: 75%;
-      left: 0;
-      opacity: 0;
-      transition: all 0.3s ease-out;
-      margin: auto;
-    }
-    a:hover::after {
-      opacity: 1;
-      top: 80%;
-      height: 2px;
-    }
+  width: 50px;
+  height: 44px;
+}
+.menu-trigger span {
+  position: absolute;
+  left: 0;
+  width: 100%;
+  height: 4px;
+  background-color: black;
+  border-radius: 4px;
+}
+.menu-trigger span:nth-of-type(1) {
+  top: 0;
+}
+.menu-trigger span:nth-of-type(2) {
+  top: 20px;
+}
+.menu-trigger span:nth-of-type(3) {
+  bottom: 0;
+}
+.menu-trigger.active {
+  -webkit-transform: rotate(360deg);
+  transform: rotate(360deg);
+}
+.menu-trigger.active span:nth-of-type(1) {
+  -webkit-transform: translateY(20px) rotate(-45deg);
+  transform: translateY(20px) rotate(-45deg);
+}
+.menu-trigger.active span:nth-of-type(2) {
+  -webkit-transform: translateY(0) rotate(45deg);
+  transform: translateY(0) rotate(45deg);
+}
+.menu-trigger.active span:nth-of-type(3) {
+  opacity: 0;
+}
+.hanburger {
+  position: absolute;
+  display: inline-block;
+  color: #fff;
+  left: 30px;
+  top: 20px;
+  z-index: 4;
+  .menuLine {
+    z-index: 5;
+    position: absolute;
+    top: 50px;
+    left: 0;
   }
 }
+.user-app {
+  font-size: 20px;
+  .user-email {
+    position: relative;
+  }
+  .userLogout {
+    position: relative;
+    right: 58px;
+    top: 5px;
+  }
+}
+
 #container-example {
   position: relative;
 
@@ -142,9 +248,9 @@ export default {
   *::before {
     box-sizing: border-box;
   }
-  .strip {
+  .menu-bar {
     width: 100%;
-    height: 100px;
+    height: 170px;
     display: flex;
     flex-wrap: wrap;
     align-items: center;
@@ -164,7 +270,7 @@ export default {
   }
 
   .colour-1 {
-    background: #a3d7ef;
+    background: #ffcc99;
     a::after {
       width: 100%;
       height: 0;
@@ -185,18 +291,43 @@ export default {
   }
 }
 .title {
-  height: 170px;
-  h1 {
-    height: 300px;
+  height: 700px;
+  width: 100%;
+  position: relative;
+  .titleName {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 700px;
+    z-index: 3;
+    display: flex;
+    flex-direction: column;
     align-items: center;
-    padding: 10px;
-    font-size: 80pt;
-    font-family: "Noto Serif JP", sans-serif;
+    justify-content: center;
+    h1 {
+      //height: 300px;
+      align-items: center;
+      padding: 10px;
+      font-size: 150pt;
+      font-family: "Noto Serif JP", sans-serif;
+      color: #ff6633;
+      font-family: "Vollkorn", serif;
+    }
+  }
+
+  .particles {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 700px;
+    z-index: 1;
   }
 }
-.user-app {
-  font-size: 40px;
-}
+// .background {
+//background-image: url("./assets/image/space2.jpg");
+//}
 .footer {
   font-size: 20px;
   font-family: sans-serif;
